154faeb3b95b83bf2ed5eadb72b1b77404af0bbd
Garrett <73368079+GareBear3602@users.noreply.github.com>
2021-10-28 11:26:39 -0400

    Streamlined Zombie Movement

--- TeamGame/Assets/Scripts/EnemyMovemnet.cs
+++ TeamGame/Assets/Scripts/EnemyMovemnet.cs
@@ -7,18 +7,37 @@ public class EnemyMovemnet : MonoBehaviour
     public float speed = 2.0f;
     private Rigidbody zombieRB;
     private GameObject player;
+    private GameObject[] players;
 
     // Start is called before the first frame update
     void Start()
     {
         zombieRB = GetComponent<Rigidbody>();
-        player = GameObject.Find("Player");
+        
+
+        players = GameObject.FindGameObjectsWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 lookDirection = (player.transform.position - transform.position).normalized;
-        //zombieRB.AddForce(lookDirection * speed);
+        float D0 = (players[0].transform.position - transform.position).sqrMagnitude;
+        float D1 = (players[1].transform.position - transform.position).sqrMagnitude;
+
+
+        if (D0 < D1)
+        {
+            player = players[0];
+        }
+        else
+        {
+            player = players[1];
+        }
+
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        zombieRB.AddForce(lookDirection * speed);
+
+
+       
     }
 }
